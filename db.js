c816ebb72d6901ffe7bbddc66e10e324cfd05d0e
i5ting <i5ting@126.com>
2016-09-14 20:19:08 +0800

    refact, please ignore

--- db.js
+++ db.js
@@ -3,7 +3,6 @@
 const env = process.env.NODE_ENV || "development";
 const config = $config.database[env]
 
-// const config = require("./config");
 var T = require("toshihiko");
 
 var toshihiko = new T.Toshihiko(config.database, config.username, config.password, {
