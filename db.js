0e54d35928e17bcdee728c7793cd3ca4054964dd
i5ting <i5ting@126.com>
2016-09-14 20:16:58 +0800

    refact, please ignore

--- db.js
+++ db.js
@@ -4,21 +4,6 @@ const env = process.env.NODE_ENV || "development";
 const config = $config.database[env]
 
 // const config = require("./config");
-const fs = require("fs");
-const path = require("path");
-const sequelize = require("sequelize");
-const modelsPath = path.join(__dirname, "/models/");
-
-// var connection = new sequelize(config.database, config.username, config.password, {
-//   host: config.host,
-//   dialect: config.dialect,
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   }
-// });
-
 var T = require("toshihiko");
 
 var toshihiko = new T.Toshihiko(config.database, config.username, config.password, {
